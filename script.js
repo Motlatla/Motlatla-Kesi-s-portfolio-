a437e6f7e03c720959ba4474828e6534a45bc8dd
Motlatla <121902066+Motlatla@users.noreply.github.com>
2026-02-24 03:31:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function hireMe(){
+
+window.location="contact.html";
+
+}
+
+
+function learnMore(){
+
+window.location="about.html";
+
+}
+
+
+function downloadCV(){
+
+alert("Downloading CV...");
+
+}
